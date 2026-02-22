d851028ce4f0b19bfdb549a98bf75c235e6ec815
PilotChalkanov <nik.chalkanov@gmail.com>
2026-02-22 16:47:10 +0200

    lcd ->guard rails for non-existing sensor, better err handling, move general method in base class

--- src/aesd_weather_app/observers/lcd.py
+++ src/aesd_weather_app/observers/lcd.py
@@ -13,22 +13,26 @@ class LCDisplay(Observer):
     def __init__(self, logger: Logger):
         self.logger = logger
         self.sensors: Dict[str, str] = {}
-    
-    def attach_sensor(self, sensor: BaseSensor):
-        self.sensors[sensor.name] = ""
 
     def update(self, sensor_name: str, data: str):
-        """Update the LCD display with new sensor data."""        
-        self.logger.info(f"LCD Display updated with {sensor_name}: {data}")
-        self.sensors[sensor_name] = data
+        """Update the LCD display with new sensor data."""
+        if sensor_name in self.sensors:       
+            self.logger.info(f"LCD Display updated with {sensor_name}: {data}")
+            self.sensors[sensor_name] = data
+        else:
+            self.logger.warning(f"Received update for unregistered sensor: {sensor_name}")
 
     def refresh_display(self):
         """Refresh the LCD display with current sensor data."""
-        with open(self.LCD_DEV, 'w') as lcd:
-            for sensor_name, data in self.sensors.items():
-                try:
+        try:
+            with open(self.LCD_DEV, 'w') as lcd:
+                for sensor_name, data in self.sensors.items():                    
                     lcd.write(f"{sensor_name}: {data}\n")
-                    self.logger.debug(f"Refreshed LCD with {sensor_name}: {data}")
-                except Exception as e:
-                    self.logger.error(f"Error updating LCD with {sensor_name}: {e}")
-                sleep(4)
+                    self.logger.debug(f"Refreshed LCD with {sensor_name}: {data}")                    
+                sleep(3)
+        except FileNotFoundError as e:
+            self.logger.error(f"LCD device not found: {e}")
+        except PermissionError as e:
+            self.logger.error(f"Permission denied when accessing LCD device: {e}")
+        except Exception as e:
+            self.logger.error(f"Unexpected error while refreshing LCD display: {e}")
