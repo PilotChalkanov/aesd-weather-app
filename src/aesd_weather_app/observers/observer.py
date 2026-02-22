3b02126261dfa9d2c54d922412db528488db321b
PilotChalkanov <nik.chalkanov@gmail.com>
2026-02-22 16:47:32 +0200

    moved method from subclass to baseclass

--- src/aesd_weather_app/observers/observer.py
+++ src/aesd_weather_app/observers/observer.py
@@ -1,7 +1,16 @@
 from abc import ABC, abstractmethod
+from typing import Dict
+
+from aesd_weather_app.sensors.base_sensor import BaseSensor
 
 
 class Observer(ABC):
+    def __init__(self):
+        self.sensors: Dict[str, str] = {}
+
     @abstractmethod
     def update(self, sensor_name, data):
         raise NotImplementedError("Subclasses must implement the update method.")
+
+    def attach_sensor(self, sensor: BaseSensor):
+        self.sensors[sensor.name] = "0.0"
